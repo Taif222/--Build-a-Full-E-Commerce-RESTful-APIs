f45884f959e858e74edce280dc4139d78b3e68b5
Taif <engtaif2022@gmail.com>
2024-07-14 01:02:29 +0300

    More Features

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
+
 const {
   getCategoryValidator,
   createCategoryValidator,
   updateCategoryValidator,
   deleteCategoryValidator,
-} = require('../utils/validators/categoryValidator');
+} = require("../utils/validators/categoryValidator");
 
 const {
   getCategories,
@@ -12,18 +13,46 @@ const {
   createCategory,
   updateCategory,
   deleteCategory,
-} = require('../services/categoryService');
+  uploadCategoryImage,
+  resizeImage,
+} = require("../services/categoryService");
+
+const authService = require("../services/authService");
+
+const subcategoriesRoute = require("./subCategoryRoute");
 
 const router = express.Router();
 
+// Nested route
+router.use("/:categoryId/subcategories", subcategoriesRoute);
+
 router
-  .route('/')
+  .route("/")
   .get(getCategories)
-  .post(createCategoryValidator, createCategory);
+  .post(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadCategoryImage,
+    resizeImage,
+    createCategoryValidator,
+    createCategory
+  );
 router
-  .route('/:id')
+  .route("/:id")
   .get(getCategoryValidator, getCategory)
-  .put(updateCategoryValidator, updateCategory)
-  .delete(deleteCategoryValidator, deleteCategory);
+  .put(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadCategoryImage,
+    resizeImage,
+    updateCategoryValidator,
+    updateCategory
+  )
+  .delete(
+    authService.protect,
+    authService.allowedTo("admin"),
+    deleteCategoryValidator,
+    deleteCategory
+  );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const path = require("path");
 
-dotenv.config({ path: 'config.env' });
-const ApiError = require('./utils/apiError');
-const globalError = require('./middlewares/errorMiddleware');
-const dbConnection = require('./config/database');
-const categoryRoute = require('./routes/categoryRoute');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
+const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
+const dbConnection = require("./config/database");
+// Routes
+const mountRoutes = require("./routes");
 
 // Connect with db
 dbConnection();
@@ -16,16 +19,17 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "uploads")));
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
 // Mount Routes
-app.use('/api/v1/categories', categoryRoute);
+mountRoutes(app);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
@@ -38,7 +42,7 @@ const server = app.listen(PORT, () => {
 });
 
 // Handle rejection outside express
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
     console.error(`Shutting down....`);
